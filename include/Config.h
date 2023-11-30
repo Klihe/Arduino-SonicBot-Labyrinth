2d2c8efff53078df7dd5dff8864b3ae034af2645
Klihe <stepan.birke@gmail.com>
2023-11-30 23:47:54 +0100

    Init PlatformIO

---
+++ include/Config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG.H
+#define CONFIG.H
+
+const int MOTOR_A = 10;
+const int MOTOR_B = 11;
+const int DIRECTION_A = 12;
+const int DIRECTION_B = 13;
+
+const int TRIGGER_1 = 2;
+const int TRIGGER_2 = 4;
+const int TRIGGER_3 = 6;
+const int ECHO_1 = 3;
+const int ECHO_2 = 5;
+const int ECHO_3 = 7;
+
+#endif
