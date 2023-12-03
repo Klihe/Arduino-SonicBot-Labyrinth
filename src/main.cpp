237bfb76b10bc46bfb0627c1be69b771e95f7aa2
Klihe <stepan.birke@gmail.com>
2023-12-03 14:00:00 +0100

    Sync sonic

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,38 @@
 #include <Robot.h>
 #include <Sensor.h>
 
-Ultrasonic ultrasonic1(TRIGGER_1, ECHO_1);
-Ultrasonic ultrasonic2(TRIGGER_2 ,ECHO_2);
-Ultrasonic ultrasonic3(TRIGGER_3, ECHO_3);
+Ultrasonic ultrasonic1(TRIGGER_FRONT, ECHO_FRONT);
+Ultrasonic ultrasonic2(TRIGGER_LEFT ,ECHO_LEFT);
+Ultrasonic ultrasonic3(TRIGGER_RIGHT, ECHO_RIGHT);
 Robot robot;
 Sensor sensor;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(MOTOR_A, OUTPUT);
-  pinMode(MOTOR_B, OUTPUT);
-  pinMode(DIRECTION_A, OUTPUT);
-  pinMode(DIRECTION_B, OUTPUT);
+  pinMode(MOTOR_LEFT, OUTPUT);
+  pinMode(MOTOR_RIGHT, OUTPUT);
+  pinMode(DIRECTION_LEFT, OUTPUT);
+  pinMode(DIRECTION_RIGHT, OUTPUT);
 }
 
 void loop() {
-  // Serial.print("Sensor 01: ");
-  // Serial.print(ultrasonic1.read(CM));
-  // Serial.println("cm");
+  int sonicReadFront = ultrasonic1.read();
+  int sonicReadLeft = ultrasonic2.read();
+  int sonicReadRight = ultrasonic3.read();
+  delay(100);
 
-  // Serial.print("Sensor 02: ");
-  // Serial.print(ultrasonic2.read(CM));
-  // Serial.println("cm");
+  Serial.print("Sensor 01: ");
+  Serial.print(ultrasonic1.read(CM));
+  Serial.println("cm");
 
-  // Serial.print("Sensor 03: ");
-  // Serial.print(ultrasonic3.read(CM));
-  // Serial.println("cm");
+  Serial.print("Sensor 02: ");
+  Serial.print(ultrasonic2.read(CM));
+  Serial.println("cm");
 
-  // delay(1000);
-  // for(int i = 0; i < 4; i++) { 
-  //   robot.straightDistance(255, 1000);
-  //   robot.turn(255, 90);
-  // }
-  // delay(1000);
-  // if(20 < ultrasonic3.read()) {
-  //   Serial.println(ultrasonic3.read());
-  //   robot.run(255);
-  // } else {
-  //   robot.stop();
-  // }
-  // delay(100);
-  sensor.untilSonic(ultrasonic3, 20, robot);
-  sensor.sideWaysSonic(ultrasonic1, ultrasonic2, 10, robot);
+  Serial.print("Sensor 03: ");
+  Serial.print(ultrasonic3.read(CM));
+  Serial.println("cm");
+
+  sensor.untilSonic(sonicReadFront, sonicReadLeft, sonicReadRight, 20, robot);
+  sensor.sideWaysSonic(sonicReadLeft, sonicReadRight, 10, robot);
 }
